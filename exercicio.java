eeea659701e900dc5cbe9b25337d57ddb3c9d055
Faustino Henriques <71952910+f50kdev@users.noreply.github.com>
2023-03-28 00:41:41 +0100

    Add files via upload

---
+++ exercicio.java
@@ -0,0 +1,21 @@
+public class app{
+
+    private String name;
+    private double saldo;
+
+    public void setname(String name){
+        this.name =name;
+
+    }
+
+    get
+
+   public static void main(String[] args){
+        
+
+    }
+}
+
+
+exercicios paginas 77
+exercicios 8.11 ate 3.17
---
+++ src/App.java
@@ -0,0 +1,20 @@
+
+/*camel case 
+ java e case sensitive
+
+MinhaClasse ==> AlunoCursEmVideo 
+meuAtributo ==> nomeAluno 
+minhavariavel ==> mediaPrimeiroBimestre
+meuMetodo ===> lancarNotaAluno 
+meupacote === alunosdocursoemvideo
+CONSTANTE
+*/
+
+
+
+public class App{
+    public static void main(String[] args){
+        System.out.println("");
+
+    }
+}
